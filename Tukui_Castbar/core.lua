cab4c5de86c57aa14242e88dba00851d33ae49f5
Krevlorne <galetto@gmx.de>
2011-02-13 18:20:07 +0100

    Added support for /moveui

--- Tukui_Castbar/core.lua
+++ Tukui_Castbar/core.lua
@@ -1,18 +1,17 @@
 -- Standalone Castbar for Tukui by Krevlorne @ EU-Ulduar
--- Credits to Tukz, Syne, Elv22 and all other great people of the Tukui community.
+-- Credits to Tukz, Syne, Elv22, Sweeper and all other great people of the Tukui community.
 
 local T, C, L = unpack(Tukui) -- Import: T - functions, constants, variables; C - config; L - locales
 
 if ( TukuiUF ~= true and ( C == nil or C["unitframes"] == nil or not C["unitframes"]["enable"] ) ) then return; end
 
---local db = TukuiCF["unitframes"]
 if (C["unitframes"].unitcastbar ~= true) then return; end
 
 local addon, ns=...
 config = ns.config
 
 local function placeCastbar(unit)
-    local font1 = TukuiCF["media"].uffont
+    local font1 = C["media"].uffont
     local castbar = nil
     local castbarpanel = nil
     
@@ -23,10 +22,24 @@ local function placeCastbar(unit)
      end
 
     local castbarpanel = CreateFrame("Frame", castbar:GetName().."_Panel", castbar)
+    local anchor = CreateFrame("Button", castbar:GetName().."_PanelAnchor", UIParent)
+    anchor:SetAlpha(0)
+    anchor:SetTemplate("Default")
+    anchor:SetBackdropBorderColor(1, 0, 0, 1)
+    anchor:SetMovable(true)
+    anchor:SetSize(250, 21)
+    anchor.text = T.SetFontString(anchor, font1, 12)
+    anchor.text:SetPoint("CENTER")
+    anchor.text:SetText(castbar:GetName())
+    anchor.text.Show = function() anchor:SetAlpha(1) end
+    anchor.text.Hide = function() anchor:SetAlpha(0) end
+    
     if unit == "player" then
-        TukuiDB.CreatePanel(castbarpanel, 250, 21, "CENTER", UIParent, 0, -200)
+        anchor:SetPoint("CENTER", UIParent, "CENTER", 0, -200)
+        castbarpanel:CreatePanel("Default", 250, 21, "CENTER", anchor, "CENTER", 0, 0)
     else
-        TukuiDB.CreatePanel(castbarpanel, 250, 21, "CENTER", UIParent, 0, -150)
+        anchor:SetPoint("CENTER", UIParent, "CENTER", 0, -150)
+        castbarpanel:CreatePanel("Default", 250, 21, "CENTER", anchor, "CENTER", 0, 0)
     end
     
     castbar:SetPoint("TOPLEFT", castbarpanel, TukuiDB.Scale(2), TukuiDB.Scale(-2))
@@ -67,32 +80,6 @@ local function placeCastbar(unit)
         TukuiTargetCastBar.Castbar.Time = castbar.time
         TukuiTargetCastBar.Castbar.Icon = castbar.icon
     end
-
-    castbarpanel:RegisterEvent("ADDON_LOADED")
-    castbarpanel:SetScript("OnEvent", function(self, event, addon)
-        self:UnregisterEvent("ADDON_LOADED")
-        
-        castbarpanel:SetMovable(true)
-        castbarpanel:EnableMouse(true)
-        castbarpanel:SetScript("OnMouseDown", function(self)
-            if button == "LeftButton" and not self.isMoving then
-                self:StartMoving();
-                self.isMoving = true;
-            end
-        end)
-        castbarpanel:SetScript("OnMouseUp", function(self)
-            if button == "LeftButton" and self.isMoving then
-                self:StopMovingOrSizing();
-                self.isMoving = false;
-            end
-        end)
-        castbarpanel:SetScript("OnHide", function(self)
-            if self.isMoving then
-                self:StopMovingOrSizing();
-                self.isMoving = false;
-            end
-        end)
-    end)
 end
 
 
@@ -104,90 +91,5 @@ if (config.separatetarget) then
     placeCastbar("target")
 end
 
-
-do
-    local castbarsUnlocked = false
-    
-    local function getPanelsForUnit(unit)
-        local castbarpanel, castbar = nil
-        
-        if unit == "player" then
-            castbarpanel = TukuiPlayerCastBar_Panel
-            castbar      = TukuiPlayerCastBar.Castbar
-        elseif unit == "target" then
-            castbarpanel = TukuiTargetCastBar_Panel
-            castbar      = TukuiTargetCastBar.Castbar
-        else
-            print("Cannot get panels for unit: " .. unit)
-            return;
-        end
-        
-        return castbarpanel, castbar
-    end
-    
-    local function unlockCastbarForUnit(unit)
-        local castbarpanel, castbar = getPanelsForUnit(unit)
-        
-        castbarpanel:Show()
-        castbar.Castbar.casting = true
-        castbar.Castbar.max = 1000
-        castbar.Castbar.duration = 1
-        castbar.Castbar.delay = 0
-        castbar.Castbar.Text:SetText(unit)
-        castbar.Castbar:Show()
-        
-        castbarpanel:RegisterForDrag("LeftButton");
-        castbarpanel:SetScript("OnDragStart", castbarpanel.StartMoving);
-        castbarpanel:SetScript("OnDragStop", castbarpanel.StopMovingOrSizing);
-    end
-    
-    local function lockCastbarForUnit(unit)
-        local castbarpanel, castbar = getPanelsForUnit(unit)
-        
-        castbar.Castbar.casting = false
-        castbarpanel:EnableMouse(false);
-    end
-    
-    local function resetCastbars()
-        local castbarpanel, _ = getPanelsForUnit("player")
-        castbarpanel:SetPoint("CENTER", UIParent, 0, -200)
-        castbarpanel, _ = getPanelsForUnit("target")
-        castbarpanel:SetPoint("CENTER", UIParent, 0, -150)
-        return
-    end
-    
-    local function TUKUICASTBARLOCK(param)
-        if param == "reset" then
-            resetCastbars()
-            return
-        end
-        
-        if castbarsUnlocked == false then
-            castbarsUnlocked = true
-            
-            if config.separateplayer then
-                unlockCastbarForUnit("player")
-            end
-            
-            if config.separatetarget then
-                unlockCastbarForUnit("target")
-            end
-            
-            print("Tukui castbar unlock")
-        elseif castbarsUnlocked == true then
-            if config.separateplayer then
-                lockCastbarForUnit("player")
-            end
-            
-            if config.separatetarget then
-                lockCastbarForUnit("target")
-            end
-            
-            castbarsUnlocked = false
-            print("Tukui castbar lock")
-        end
-    end
-    
-    SLASH_TUKUICASTBAR1 = "/tcb"
-    SlashCmdList["TUKUICASTBAR"] = TUKUICASTBARLOCK
-end
+table.insert(T.MoverFrames, TukuiPlayerCastBar_PanelAnchor)
+table.insert(T.MoverFrames, TukuiTargetCastBar_PanelAnchor)
